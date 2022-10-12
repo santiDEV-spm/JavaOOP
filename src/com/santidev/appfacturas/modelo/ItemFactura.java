dc90c50479ed1ae059bd7996ad0d3f77aa63a2a4
SantiDev <androidsan94@gmail.com>
2022-10-12 16:14:10 -0500

    first

--- src/com/santidev/appfacturas/modelo/ItemFactura.java
+++ src/com/santidev/appfacturas/modelo/ItemFactura.java
@@ -3,6 +3,12 @@ package com.santidev.appfacturas.modelo;
 public class ItemFactura {
 
     private int cantidad;
+    private Producto producto;
+
+    public ItemFactura(int cantidad, Producto producto) {
+        this.cantidad = cantidad;
+        this.producto = producto;
+    }
 
     public int getCantidad() {
         return cantidad;
@@ -10,5 +16,13 @@ public class ItemFactura {
 
     public void setCantidad(int cantidad) {
         this.cantidad = cantidad;
+    }
+
+    public Producto getProducto() {
+        return producto;
+    }
+
+    public void setProducto(Producto producto) {
+        this.producto = producto;
     }
 }
--- src/com/santidev/appfacturas/modelo/Producto.java
+++ src/com/santidev/appfacturas/modelo/Producto.java
@@ -4,6 +4,11 @@ public class Producto {
     private int codigo;
     private String nombre;
     private float precio;
+    private static int ultimoCodigo;
+
+    public Producto() {
+        this.codigo = ++ultimoCodigo;
+    }
 
     public int getCodigo() {
         return codigo;
